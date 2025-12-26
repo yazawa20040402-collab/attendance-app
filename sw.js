3349f0e601fc94b12e3a1de959e647cf3fc25881
yazawa20040402-collab <yazawa20040402@gmail.com>
2025-12-26 17:00:15 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "attendance-pwa-v1";
+const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+});
